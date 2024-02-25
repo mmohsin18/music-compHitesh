a7c7955eb17002309539395467e90cb4904ad73f
mmohsin18 <mmohsin0804@gmail.com>
2024-02-25 21:39:05 +0600

    class 6: grid bg TestimonialCards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChoseUs from "@/components/WhyChoseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
         <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <HeroSection />
             <FeaturedCourses/>
+            <WhyChoseUs/>
+            <TestimonialCards />
         </main>
     );
 }
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -35,7 +35,7 @@ function FeaturedCourses() {
                   <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                       <p className="text-lg font-bold sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
                       <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
-                      <Link className="border-2 rounded-md px-4 py-2 mt-3" href={`/courses/${course.slug}`}>Learn More</Link>
+                      <Link className="border-2 rounded-md text-white px-4 py-2 mt-3" href={`/courses/${course.slug}`}>Learn More</Link>
                   </div>
                 </BackgroundGradient>
               </div>
